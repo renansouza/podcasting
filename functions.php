0a99de4f03353fbecabf0c617f689846b4d9cb3d
Renan Souza <falecomrenansouza@gmail.com>
2017-07-16 20:18:38 -0300

    Configurações bases do tema

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php 
+
+// Remove a meta wp-generator
+remove_action('wp_head', 'wp_generator');
--- header.php
+++ header.php
@@ -4,9 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php bloginfo('name'); ?></title>
-	
 
 	<?php wp_head();?>
 </head>
 <body>
-	
+	 
